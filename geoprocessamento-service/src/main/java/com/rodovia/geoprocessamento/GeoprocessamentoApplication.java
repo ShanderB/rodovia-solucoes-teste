7eef8142ffa775a22dd4a52fd4b9f4f630c8031f
ShanderB <s.hander8910@hotmail.com>
2024-04-18 18:14:37 -0300

    Commit inicial.

---
+++ geoprocessamento-service/src/main/java/com/rodovia/geoprocessamento/GeoprocessamentoApplication.java
@@ -0,0 +1,13 @@
+package com.rodovia.geoprocessamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoprocessamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoprocessamentoApplication.class, args);
+	}
+
+}
---
+++ geoprocessamento-service/src/test/java/com/rodovia/geoprocessamento/GeoprocessamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodovia.geoprocessamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoprocessamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
