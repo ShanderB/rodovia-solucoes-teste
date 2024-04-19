a81864777a5df010065c475e49864c1355d0d03a
ShanderB <s.hander8910@hotmail.com>
2024-04-19 15:29:31 -0300

    Removido import não utilizado.

--- geoprocessamento-service/src/main/java/com/rodovia/geoprocessamento/rest/Rest.java
+++ geoprocessamento-service/src/main/java/com/rodovia/geoprocessamento/rest/Rest.java
@@ -3,7 +3,6 @@ package com.rodovia.geoprocessamento.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
