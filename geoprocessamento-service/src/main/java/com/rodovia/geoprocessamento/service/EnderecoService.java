213c1ae3036a068c592ac6c2fc146bf5ec180aa3
ShanderB <s.hander8910@hotmail.com>
2024-04-18 23:13:34 -0300

    Criação e integração com o banco de dados para salvar cada request.

--- geoprocessamento-service/src/main/java/com/rodovia/geoprocessamento/service/EnderecoService.java
+++ geoprocessamento-service/src/main/java/com/rodovia/geoprocessamento/service/EnderecoService.java
@@ -1,10 +1,18 @@
 package com.rodovia.geoprocessamento.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rodovia.geoprocessamento.DTO.InformacoesEnderecoDTO;
+import com.rodovia.geoprocessamento.repository.EnderecoRepository;
 
 @Service
 public class EnderecoService {
@@ -12,30 +20,37 @@ public class EnderecoService {
     @Value("${api.endpoint.latlong}")
     private String apiEndpoint;
 
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
     public ResponseEntity<String> getEndereco(Double latitude, Double longitude) {
+        ResponseEntity<String> response;
+        try {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiEndpoint)
                 .queryParam("format", "json")
                 .queryParam("lat", latitude)
                 .queryParam("lon", longitude);
 
-        ResponseEntity<String> response = new RestTemplate().getForEntity(builder.toUriString(), String.class);
+            response = new RestTemplate().getForEntity(builder.toUriString(), String.class);
+
+            saveEndereco(response.getBody());
+
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar endereço: " + e.getMessage());
+            response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
         return response;
     }
 
-   /*InformacoesEnderecoDTO endereco = InformacoesEnderecoDTO.builder()
-    .id(1)
-    .latitude(40.7128)
-    .longitude(-74.0060)
-    .enderecoFormatado("123 Main St, New York, NY")
-    .rua("Main St")
-    .bairro("Manhattan")
-    .cidade("New York")
-    .estado("NY")
-    .pais("USA")
-    .cep("10001")
-    .latitutParam(40.7128)
-    .longitudeParam(-74.0060)
-    .dataHoraOperacao(new Date())
-    .build();*/
+    private void saveEndereco(String responseBody) throws JsonMappingException, JsonProcessingException {
+        try {
+            InformacoesEnderecoDTO endereco = new ObjectMapper().readValue(responseBody, InformacoesEnderecoDTO.class);
+
+            enderecoRepository.save(endereco);
+        } catch (JsonProcessingException e) {
+            System.out.println("Erro ao inserir no banco: " + e.getMessage());
+            }
+    }
 
 }
