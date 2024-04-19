da754d0a666698bf15883cb260bf759919fc7e0f
ShanderB <s.hander8910@hotmail.com>
2024-04-19 20:42:57 -0300

    Criado interface de endereço.

---
+++ webapp-geoprocessamento/src/app/geoprocessamento/interfaces/Endereco.ts
@@ -0,0 +1,16 @@
+export interface Address {
+    suburb: string;
+    city: string;
+    state: string;
+    postcode: string;
+    country: string;
+    latitudeUsadoRequest: number;
+    longitudeUsadoRequest: number;
+}
+
+export interface Endereco {
+    address: Address;
+    display_name: string;
+    latitudeUsadoRequest: number;
+    longitudeUsadoRequest: number;
+}
