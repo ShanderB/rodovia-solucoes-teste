6c10839fd862a8ad60d7b69e1706c6a7a10f5571
ShanderB <s.hander8910@hotmail.com>
2024-04-20 12:51:00 -0300

    Criação de testes para a service.

--- webapp-geoprocessamento/src/app/geoprocessamento/geoprocessamento.service.spec.ts
+++ webapp-geoprocessamento/src/app/geoprocessamento/geoprocessamento.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { GeoprocessamentoService } from './geoprocessamento.service';
+import { Endereco } from './interfaces/Endereco';
+import { environment } from '@env/environment';
 
 describe('GeoprocessamentoService', () => {
   let service: GeoprocessamentoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GeoprocessamentoService]
+    });
+
     service = TestBed.inject(GeoprocessamentoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('deve ser criado', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('deve buscar endereço por coordenadas', () => {
+    const dummyEndereco: Endereco = {
+      latitudeUsadoRequest: -23.5505,
+      longitudeUsadoRequest: -46.6333,
+    };
+
+    service.getEnderecoPorCoordenadas(-23.5505, -46.6333)
+
+    const req = httpMock.expectOne(`${environment.apiURL}?latitude=-23.5505&longitude=-46.6333`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyEndereco);
   });
 });
