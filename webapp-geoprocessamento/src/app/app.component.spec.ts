a3476244fa6b51c5d33e1e6dfa9a83ca358ba0c8
ShanderB <s.hander8910@hotmail.com>
2024-04-20 12:51:03 -0300

    Ajuste de lint.

--- webapp-geoprocessamento/src/app/app.component.spec.ts
+++ webapp-geoprocessamento/src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'webapp-geoprocessamento' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('webapp-geoprocessamento');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
