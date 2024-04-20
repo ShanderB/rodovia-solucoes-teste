d3aceffe9868c25172fbf4c7387027e7ee073abd
ShanderB <s.hander8910@hotmail.com>
2024-04-19 22:12:15 -0300

    Adicionando ngDestroy.

--- webapp-geoprocessamento/src/app/geoprocessamento/geoprocessamento.component.ts
+++ webapp-geoprocessamento/src/app/geoprocessamento/geoprocessamento.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { Endereco } from './interfaces/Endereco';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-geoprocessamento',
@@ -13,16 +14,24 @@ import { Endereco } from './interfaces/Endereco';
   styleUrl: './geoprocessamento.component.css'
 })
 export class GeoprocessamentoComponent {
+  ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor(private geoprocessamentoService: GeoprocessamentoService) { }
   hasData = false;
   localizacaoEnderecoInformacao: Endereco = {} as Endereco;
 
   ngOnInit() {
-    this.geoprocessamentoService.coordenadasAtualizadas.subscribe(responseEndereco => {
+    this.geoprocessamentoService.coordenadasAtualizadas
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe(responseEndereco => {
       this.localizacaoEnderecoInformacao = responseEndereco;
       this.hasData = true;
     });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
   getEnderecoPorCoordenadas() {
